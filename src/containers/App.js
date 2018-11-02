4297edda3fd4ef9a278184e8b11e82bc85f2cfdc
alex <hamster2240@gmail.com>
2018-11-02 22:02:42 +0300

    Add pagination component to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,18 +3,59 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import Content from '../components/Content';
+import Pagination from '../components/Pagination';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Sidebar />
-        <Content />
-      </div>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         <Sidebar />
+//         <Content />
+//       </div>
+//     );
+//   }
+// }
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        // an example array of items to be paged
+        var exampleItems = require('../data/vacancies.json');
+
+        this.state = {
+            exampleItems: exampleItems,
+            pageOfItems: []
+        };
+
+        // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
+        this.onChangePage = this.onChangePage.bind(this);
+    }
+
+    onChangePage(pageOfItems) {
+        // update state with new page of items
+        this.setState({ pageOfItems: pageOfItems });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="text-center">
+                              <Header />
+                              <Sidebar />
+                             <Content />
+                        {/* {this.state.pageOfItems.map(item =>
+                            <div key={item.id}>{item.name}</div>
+                        )} */}
+                        <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
