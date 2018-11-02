83118500e53e0e7f5d18b61b8cf1e137ec84dd75
alex <hamster2240@gmail.com>
2018-11-02 10:36:10 +0300

    Add sidebar links

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -8,8 +8,9 @@ const assets = require('../assets.js');
 const Sidebar = (props) => {
     return (
       <div className="sidebar">
-        <LinkWithImg text="startpage" linkClass="sidebar__link" imagePath={assets.homeIcon} imgAlt="logout" imgClass="sidebar__img sidebar__img--startpage"/>
-        <LinkWithImg text="logout" linkClass="sidebar__link" imagePath={assets.logoutIcon} imgAlt="logout" imgClass="sidebar__img sidebar__img--logout"/>
+        <LinkWithImg text="" linkClass="sidebar__link sidebar__link--startpage" imagePath={assets.homeIcon} imgAlt="startpage" imgClass="sidebar__img sidebar__img--startpage"/>
+        <LinkWithImg text="" linkClass="sidebar__link sidebar__link--settings" imagePath={assets.settingsIcon} imgAlt="settings" imgClass="sidebar__img sidebar__img--settings"/>
+        <LinkWithImg text="" linkClass="sidebar__link sidebar__link--logout" imagePath={assets.logoutIcon} imgAlt="logout" imgClass="sidebar__img sidebar__img--logout"/>
       </div>
     );
 }
