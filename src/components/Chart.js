079bef64740bb98d7ae46498bcf4fede2a28ce85
alex <hamster2240@gmail.com>
2018-11-03 21:14:31 +0300

    Make Card component looks more readable

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import Card from '../components/Card';
 import './Chart.css';
 
 const data = require('../data/vacancies.json');
 
 const dataComponent = data.map((item, i) => {
+  return (
+    <Card
+      key={i.toString()}
+      position={item.position}
+      company={item.companyName}
+      salary={item.salary}
+      status={item.status}
+      />
+  )
 })
 const Chart = () => {
   return (
