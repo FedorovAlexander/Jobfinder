409cc3bd8a5484f3fff85fa21b1370db727fb1fa
alex <hamster2240@gmail.com>
2018-11-03 21:16:46 +0300

    Create SearchBox Component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import SearchBox from './SearchBox';
 import './Filters.css';
 
 class Filters extends Component {
   render() {
     return (
       <section className="filters">
-    </section>
+        <SearchBox />
+      </section>
     );
   }
 }
