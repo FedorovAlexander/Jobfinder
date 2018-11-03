b5d69116a2f4c894dc2faa0190e064d26adde97f
alex <hamster2240@gmail.com>
2018-11-03 23:40:30 +0300

    Add pagination to the Chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Card from '../components/Card';
+import Pagination from '../components/Pagination';
 import './Chart.css';
 
 const data = require('../data/vacancies.json');
 
-const dataComponent = data.map((item, i) => {
-  return (
-    <Card
-      key={i.toString()}
-      position={item.position}
-      company={item.companyName}
-      salary={item.salary}
-      status={item.status}
-      />
-  )
-})
-const Chart = () => {
-  return (
-    <section className="chart">
+class Chart extends Component {
+  constructor() {
+    super();
+
+    var data = require('../data/vacancies.json');
+
+    var dataItems = [...data].map((item) => ({
+      id: item.id,
+      position: item.position,
+      companyName: item.companyName,
+      salary: item.salary,
+      status: item.status
+    }));
+    this.state = {
+      data: data,
+      pageOfItems: [],
+      searchfield: ''
+    };
+
+    this.onChangePage = this.onChangePage.bind(this);
+  }
+
+  onChangePage(pageOfItems) {
+
+    this.setState({pageOfItems: pageOfItems});
+  }
+
+  render() {
+    return (<section className="chart">
       <h1 className="chart__title">Chart</h1>
-    {/* <Pagination /> */}
-
-    {dataComponent}
-    </section>
-  );
+      {this.state.pageOfItems.map(item => (
+        <Card key={item.id}
+          position={item.position}
+          company={item.companyName}
+          salary={item.salary}
+          status={item.status}/>
+      ))}
+      <Pagination
+         items={this.state.data}
+         onChangePage={this.onChangePage}/>
+    </section>);
+  }
 }
-
 export default Chart;
