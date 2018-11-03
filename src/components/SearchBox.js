aaec4ea1878de7a1e21d7819fcb823cc95733e10
alex <hamster2240@gmail.com>
2018-11-03 21:27:47 +0300

    Change placeholder in SearcBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBox = () => {
   return (
-    <input type='search' placeholder='search robots' />
+    <input type='search' placeholder='search vacancies' />
   );
 }
 
