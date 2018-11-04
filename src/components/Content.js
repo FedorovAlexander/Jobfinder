2fc5bf6d8c6b2b350abf0303f22f6742bb431708
alex <hamster2240@gmail.com>
2018-11-04 12:57:13 +0300

    Remove data variable from content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,13 +3,11 @@ import Filters from './Filters';
 import Chart from './Chart';
 import './Content.css';
 
-const data = require('../data/vacancies.json');
-
 class Content extends Component {
   render() {
     return (
       <section className="content">
-          <Chart data={data} />
+          <Chart/>
         <Filters />
       </section>
     );
