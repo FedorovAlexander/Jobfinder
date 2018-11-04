913357c3e426bb9a6affa11490be905123b97136
alex <hamster2240@gmail.com>
2018-11-04 20:07:10 +0300

    Set state to Filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,10 +4,22 @@ import './Filters.css';
 import {data} from '../data/vacancies';
 
 class Filters extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: data,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange(event) {
+    console.log(event.target.value)
+  }
+
   render() {
     return (
       <section className="filters">
-        <SearchBox items={data} />
+        <SearchBox searchChange={this.onSearchChange} />
       </section>
     );
   }
