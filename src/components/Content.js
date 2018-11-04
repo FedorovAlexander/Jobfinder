db233df2bfcc960609fb99da780d45bc95a17460
alex <hamster2240@gmail.com>
2018-11-04 20:06:46 +0300

    Set state to Content compoent

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import Filters from './Filters';
 import Chart from './Chart';
 import './Content.css';
+import {data} from '../data/vacancies'
 
 class Content extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: data,
+      pageOfItems: [],
+      searchfield: ''
+    };
+
+    this.onChangePage = this.onChangePage.bind(this);
+  }
+  onChangePage(pageOfItems) {
+
+    this.setState({pageOfItems: pageOfItems});
+  }
+
   render() {
     return (
       <section className="content">
-          <Chart/>
+          <Chart data={this.state.data}/>
         <Filters />
       </section>
     );
