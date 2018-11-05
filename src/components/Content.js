ae5bfdcd440b792c8d839e4533bc6f8142ffe005
alex <hamster2240@gmail.com>
2018-11-05 10:58:16 +0300

    Remove constructor from content

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,38 +2,14 @@ import React, { Component } from 'react';
 import Filters from './Filters';
 import Chart from './Chart';
 import './Content.css';
-import {data} from '../data/vacancies'
 
 class Content extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      data: data,
-      pageOfItems: [],
-      searchfield: ''
-    };
-
-    this.onChangePage = this.onChangePage.bind(this);
-  }
-  onChangePage(pageOfItems) {
-
-    this.setState({pageOfItems: pageOfItems});
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-    const filteredData = this.state.data.filter(dataItem => {
-      return dataItem.position.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
-    console.log(filteredData)
-  }
 
   render() {
 
     return (
       <section className="content">
-          <Chart data={this.state.data}/>
+          <Chart />
         <Filters />
       </section>
     );
