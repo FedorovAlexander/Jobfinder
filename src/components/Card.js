5ca32a9e15c200b779f2253677e9838809bb7b42
alex <hamster2240@gmail.com>
2018-11-05 16:39:08 +0300

    Add links to the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({ position, company, salary, status }) => {
+
   return (
     <div className="card">
+      <div className="card__img-block">
+        {position.charAt(0)}
+      </div>
       <div className="card__position-company-block">
-        <h2 className="card__position">{position}</h2>
-      <p className="card__company">{company}</p>
+        <h2 className="card__position">
+          <a href="/" className="card__link card__link--position">
+            {position}
+          </a>
+        </h2>
+      <p className="card__company">
+        <a href="/" className="card__link card__link--company">
+        {company}          
+        </a>
+      </p>
       </div>
       <div className="card__salary-status-block">
         <span className="card__salary">{salary}</span>
