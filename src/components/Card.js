3f834b5b4001aa5f39f2382e070b7de0f7ac15e5
alex <hamster2240@gmail.com>
2018-11-06 08:46:03 +0300

    Change card__salary inner HTML

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,13 +16,12 @@ const Card = ({ position, company, salary, status }) => {
         </h2>
       <p className="card__company">
         <a href="/" className="card__link card__link--company">
-        {company}          
+        {company}
         </a>
       </p>
       </div>
       <div className="card__salary-status-block">
-        <span className="card__salary">{salary}</span>
-      <p className={"card__status"}>{status}</p>
+        <span className="card__salary">{"$" + salary + "/year"}</span>
       </div>
     </div>
   )
