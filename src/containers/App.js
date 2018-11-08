118bb14d41f4026bfc8f183aa0109e994ec31374
alex <hamster2240@gmail.com>
2018-11-08 23:21:45 +0300

    Add file routing to App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import Content from '../components/Content';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import ChartPage from '../components/ChartPage';
+import Board from '../components/Board';
+import User from '../components/User';
+import Vacancy from '../components/Vacancy';
+import Company from '../components/Company';
+import Login from '../components/Login';
+import Registration from '../components/Registration';
 
 class App extends Component {
   render() {
     return (
-      <div className="App__container">
-        <Header />
-        <Content />
-      </div>
+      <BrowserRouter>
+        <>
+          <div>
+            <Route path="/" component={ChartPage} exact />
+            <Route path="/board" component={Board} />
+            <Route path="/user" component={User} />
+            <Route path="/vacancy" component={Vacancy} />
+            <Route path="/company" component={Company} />
+            <Route path="/login" component={Login} />
+            <Route path="/registration" component={Registration} />
+          </div>
+        </>
+      </BrowserRouter>
     );
   }
 }
