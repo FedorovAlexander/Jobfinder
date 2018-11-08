757cad2352e830badfbb8dd8f2da6599cc4c4da5
alex <hamster2240@gmail.com>
2018-11-08 23:24:43 +0300

    Change a tags to divs in LinkWithImg component

--- src/components/LinkWithImg.js
+++ src/components/LinkWithImg.js
@@ -4,10 +4,10 @@ import '../assets'
 
 function LinkWithImg({ text, textClass, imagePath, linkClass, imgClass, imgAlt }) {
   return (
-    <a href="/" className={linkClass}>
+    <div className={linkClass}>
     <img src={imagePath} className={imgClass} alt={imgAlt}/>
       <span className={textClass}>{text}</span>
-    </a>
+    </div>
   )
 }
 
