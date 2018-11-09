9577154a973c02923b58a422424b0c835b9762c7
alex <hamster2240@gmail.com>
2018-11-09 07:01:48 +0300

    Create VacancyContent component

---
+++ src/components/VacancyContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './VacancyContent.css';
+
+
+
+const VacancyContent = () => {
+  return (
+    <section className="vacancy-content">
+          <h1 className="vacancy-content__title">Vacancy</h1>
+    </section>
+  )
+}
+
+export default VacancyContent;
