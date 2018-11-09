a427e864fea260e5efcc50a71f3e1402b8a46b1d
alex <hamster2240@gmail.com>
2018-11-09 14:52:47 +0300

    Set vacancy-content title tag to h2

--- src/components/VacancyContent.js
+++ src/components/VacancyContent.js
@@ -9,7 +9,7 @@ function VacancyContent ({vacancyTitle, vacancyDescription}) {
   return (
     <section className="vacancy-content">
           <PageTitle titleClass="page-title" titleText="Vacancy" />
-          <h3 className="vacancy-content__desc-title">Job Description</h3>
+          <h2 className="vacancy-content__desc-title">Job Description</h2>
           <p className="vacancy-content__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor neque, suscipit ac accumsan eget, sollicitudin sit amet urna. Mauris ut ex risus. Aliquam eleifend augue diam, eget congue ex bibendum vitae. Nulla condimentum maximus nisl non venenatis. Fusce vitae dui erat. Suspendisse in consectetur libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
 
           Quisque feugiat ut orci ac pretium. Vestibulum vitae mi iaculis, mattis metus at, commodo libero. Vivamus at placerat nulla, nec lobortis arcu. Phasellus ultrices quam sed dolor eleifend, sed interdum est pharetra. Donec nec dolor eu nisl auctor ullamcorper. Vestibulum laoreet erat sit amet ultricies tristique. Vestibulum eu dolor volutpat, malesuada nibh eget, pulvinar ante. Vivamus euismod mattis erat in eleifend. Morbi et dui vitae libero volutpat aliquet a quis ipsum. Maecenas iaculis augue nec lacus aliquam, sit amet posuere dolor ullamcorper. Nullam ac aliquam elit, ut molestie diam.</p>
