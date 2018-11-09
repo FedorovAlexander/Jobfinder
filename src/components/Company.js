d1445ba4095686514ee036d09c4ca2331a1c6c7f
alex <hamster2240@gmail.com>
2018-11-09 19:54:16 +0300

    Add Company page

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import CompanyContent from './CompanyContent';
+import './Company.css';
 
-
-const Company = () => {
-  return (
-    <div>
-      company
-      lorem ipsum dolor emet
-    </div>
-  )
+class Company extends Component {
+  render() {
+    return (
+        <div>
+          <Header />
+          <section className="company">
+            <div className="company__wrapper">
+              <div className="company__container">
+          <CompanyContent />
+        </div>
+        <Footer />
+      </div>
+    </section>
+        </div>
+    );
+  }
 }
 
 export default Company;
