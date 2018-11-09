20689e101246eed67e30eb37eaeaaf851e9d65ff
alex <hamster2240@gmail.com>
2018-11-09 16:10:15 +0300

    Conditions styles

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import "./ItemsList.css";
 import ListItem from "./ListItem";
+const assets = require('../assets.js');
 
 const ItemsList = () => {
   return (
     <section className="conditions">
       <h2 className="conditions__title">Conditions</h2>
       <ul className="conditions__list">
-        <ListItem itemTitle="Salary" itemValue="value" />
-        <ListItem itemTitle="Status" itemValue="value" />
-        <ListItem itemTitle="Work Time" itemValue="value" />
-        <ListItem itemTitle="Road Time" itemValue="value" />
-        <ListItem itemTitle="Education" itemValue="value" />
-        <ListItem itemTitle="Interesting" itemValue="value" />
+        <ListItem imgSrc={assets.salaryIcon} itemTitle="Salary" itemValue="value" />
+        <ListItem imgSrc={assets.statusIcon} itemTitle="Status" itemValue="value" />
+        <ListItem imgSrc={assets.timeIcon} itemTitle="Work Time" itemValue="value" />
+        <ListItem imgSrc={assets.roadIcon} itemTitle="Road Time" itemValue="value" />
+        <ListItem imgSrc={assets.educationIcon} itemTitle="Education" itemValue="value" />
+        <ListItem imgSrc={assets.intIcon} itemTitle="Interesting" itemValue="value" />
       </ul>
     </section>
   )
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './ListItem.css';
 
-function ListItem({ itemTitle, itemValue }) {
+function ListItem({ itemTitle, itemValue, imgSrc }) {
   return (
       <li className="conditions__list-item">
-        <h3 className="coditions__item-title">{itemTitle}</h3>
+        <img src ={imgSrc} alt="icon" className="coditions__item-img" />
+        <h3 className="conditions__item-title">{itemTitle}</h3>
         <p className="conditions__item-value">{itemValue}</p>
       </li>
   )
