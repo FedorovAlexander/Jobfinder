75ebdd11d5a57bbc337fd75ba6d2ffb134e84de6
alex <hamster2240@gmail.com>
2018-11-09 13:36:01 +0300

    Change vacancy-content markup

--- src/components/VacancyContent.js
+++ src/components/VacancyContent.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './VacancyContent.css';
+import ItemsList from './ItemsList'
+import Comments from './Comments'
+import PageTitle from './PageTitle'
 
 
-
-const VacancyContent = () => {
+function VacancyContent ({vacancyTitle, vacancyDescription}) {
   return (
     <section className="vacancy-content">
-          <h1 className="vacancy-content__title">Vacancy</h1>
+          <PageTitle titleClass="page-title" titleText="Vacancy" />
+          <h3 className="vacancy-content__desc-title">Job Description</h3>
+          <p className="vacancy-content__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor neque, suscipit ac accumsan eget, sollicitudin sit amet urna. Mauris ut ex risus. Aliquam eleifend augue diam, eget congue ex bibendum vitae. Nulla condimentum maximus nisl non venenatis. Fusce vitae dui erat. Suspendisse in consectetur libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+          Quisque feugiat ut orci ac pretium. Vestibulum vitae mi iaculis, mattis metus at, commodo libero. Vivamus at placerat nulla, nec lobortis arcu. Phasellus ultrices quam sed dolor eleifend, sed interdum est pharetra. Donec nec dolor eu nisl auctor ullamcorper. Vestibulum laoreet erat sit amet ultricies tristique. Vestibulum eu dolor volutpat, malesuada nibh eget, pulvinar ante. Vivamus euismod mattis erat in eleifend. Morbi et dui vitae libero volutpat aliquet a quis ipsum. Maecenas iaculis augue nec lacus aliquam, sit amet posuere dolor ullamcorper. Nullam ac aliquam elit, ut molestie diam.</p>
+          <ItemsList />
+          <Comments />
     </section>
   )
 }
