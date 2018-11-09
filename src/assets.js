b003a22a4f74de740792687e15dc3e001db7f0ab
alex <hamster2240@gmail.com>
2018-11-09 16:09:03 +0300

    Add icons links to the assets file

--- src/assets.js
+++ src/assets.js
@@ -4,6 +4,12 @@ const assetsObject = {
   userIcon: require('./img/user.svg'),
   settingsIcon: require('./img/settings.svg'),
   chartIcon: require('./img/graph.svg'),
-  boardIcon: require('./img/board.svg')
+  boardIcon: require('./img/board.svg'),
+  educationIcon: require('./img/brain.svg'),
+  statusIcon: require('./img/briefcase.svg'),
+  roadIcon: require('./img/bus.svg'),
+  salaryIcon: require('./img/money.svg'),
+  intIcon: require('./img/question.svg'),
+  timeIcon: require('./img/time.svg')
 }
 module.exports = assetsObject
