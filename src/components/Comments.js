4d8667ef760dfb769ba9452fa0ed81fa595c1141
alex <hamster2240@gmail.com>
2018-11-09 16:09:42 +0300

    Set test text to comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -5,7 +5,7 @@ function Comments({ commentsText }) {
   return (
       <section className="comments">
         <h2 className="coditions__item-title">Comments</h2>
-        <p className="conditions__item-value">{commentsText}</p>
+        <p className="comments__item">Test{commentsText}</p>
       </section>
   )
 }
