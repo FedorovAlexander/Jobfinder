4f5497e4bc1a750c436a12d6399bf1d53c59f939
alex <hamster2240@gmail.com>
2018-11-10 19:19:13 +0300

    New vacancy component styles

--- src/components/NewVacancy/NewVacancy.js
+++ src/components/NewVacancy/NewVacancy.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import Header from '../Header/Header';
 import NewVacancyForm from '../NewVacancyForm/NewVacancyForm';
-import '../NewVacancy/NewVacancy.css';
+import Footer from '../Footer/Footer';
+import './NewVacancy.css';
 
-const Login = () => {
+
+const NewVacancy = () => {
   return (
     <div>
-      <div className="new-vacancy">
-        <section className="new-vacancy__form">
-          <NewVacancyForm />
-        </section>
-      </div>
+      <Header />
+        <section className="new-vacancy">
+          <div className="new-vacancy__wrapper">
+            <div className="new-vacancy__container">
+              <NewVacancyForm />
+          </div>
+          <Footer />
+        </div>
+
+      </section>
+
     </div>
   )
 }
 
-export default Login;
+export default NewVacancy;
