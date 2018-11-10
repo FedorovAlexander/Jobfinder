a026a05b456ed2681aa6d775f672d679604a64b9
alex <hamster2240@gmail.com>
2018-11-10 18:23:12 +0300

    Mke LoginForm code more readable

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -9,9 +9,21 @@ const LoginForm = () => {
       <section className="log-form">
         <h1 className="log-form__title">Log In</h1>
         <form>
-          <InputWithLabel type="email" name="email" placeholder="Email" />
-          <InputWithLabel type="password" name="password" placeholder="Password" />
-          <input className ="log-form__sumbit" type="submit" value="Submit" formAction="/chartPage" />
+          <InputWithLabel
+            type="email"
+            name="email"
+            placeholder="Email"
+            inputClass="input-with-label input-with-label--login-reg"
+          />
+          <InputWithLabel
+            type="password"
+            name="password"
+            placeholder="Password" inputClass="input-with-label input-with-label--login-reg"
+          />
+          <input
+            className ="log-form__sumbit"
+            type="submit" value="Submit" formAction="/chartPage"
+          />
           <Link text="Do not have an account?" linkClass="log-form__forgot" path="/registration"/>
         </form>
 
