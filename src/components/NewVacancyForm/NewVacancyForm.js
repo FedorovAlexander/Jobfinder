8ed208c57d9acf1531e858107ad1bde1825ce4af
alex <hamster2240@gmail.com>
2018-11-10 20:05:57 +0300

    New Vacancy form markup

--- src/components/NewVacancyForm/NewVacancyForm.js
+++ src/components/NewVacancyForm/NewVacancyForm.js
@@ -1,23 +1,80 @@
 import React from 'react';
 import InputWithLabel from '../InputWithLabel/InputWithLabel';
+import CustomCheckbox from '../CustomCheckbox/CustomCheckbox';
+import FormButton from '../FormButton/FormButton';
 import Link from '../Link/Link';
 import '../NewVacancyForm/NewVacancyForm.css';
 
 const NewVacancyForm = () => {
   return (
-    <div>
-      <section className="new-vacancy-form">
-        <h1 className="new-vacancy-form__title">Log In</h1>
-        <form>
-          {/* <InputWithLabel type="email" name="email" placeholder="Email" />
-          <InputWithLabel type="password" name="password" placeholder="Password" />
-          <input className ="new-vacancy-form__sumbit" type="submit" value="Submit" formAction="/chartPage" />
-          <Link text="Do not have an account?" linkClass="new-vacancy-form__forgot" path="/registration"/> */}
-          
-        </form>
 
-      </section>
-    </div>
+    <section className="new-vacancy-form">
+      <h1 className="new-vacancy-form__title">New Vacancy</h1>
+      <form>
+        <InputWithLabel
+          type="text"
+          name="title"
+          placeholder="Title"
+          inputClass="input-with-label input-with-label--new-vacancy"
+        />
+        <InputWithLabel
+          type="text"
+          name="company"
+          placeholder="Company" inputClass="input-with-label input-with-label--new-vacancy"
+        />
+        <div className="new-vacancy-form__options-block">
+          <InputWithLabel
+            type="text"
+            name="salary"
+            placeholder="Salary" inputClass="input-with-label input-with-label--new-vacancy input-with-label--option"
+          />
+          <InputWithLabel
+            type="text"
+            name="Work Time"
+            placeholder="Work Time" inputClass="input-with-label input-with-label--new-vacancy input-with-label--option"
+          />
+          <InputWithLabel
+            type="text"
+            name="Road Time"
+            placeholder="Road Time" inputClass="input-with-label input-with-label--new-vacancy input-with-label--option"
+          />
+        </div>
+        <div className="new-vacancy-form__checkbox-block">
+          <CustomCheckbox
+            blockClass="new-vacancy-form__checkbox"
+            labelClass="checkbox-label checkbox-label--log-reg"
+            labelText="Interesting"
+            inputClass="checkbox-input"
+            checkmarkClass="checkmark checkmark--vac"
+            inputId="interest"
+           />
+          <CustomCheckbox
+            blockClass="new-vacancy-form__checkbox"
+            labelClass="checkbox-label checkbox-label--log-reg"
+            labelText="Requires Education"
+            inputClass="checkbox-input"
+            checkmarkClass="checkmark checkmark--vac"
+            inputId="education"
+           />
+        </div>
+        <div className="new-vacancy-form__comments-block">
+
+        </div>
+        <div className="new-vacancy-form__buttons-block">
+          <FormButton
+            buttonClass="new-vacancy-form__button new-vacancy-form__button--submit"
+            text="Submit" path="/chartPage"
+          />
+
+          <FormButton
+            buttonClass="new-vacancy-form__button new-vacancy-form__button--cancel"
+            text="Cancel" path=""
+          />
+        </div>
+      </form>
+
+    </section>
+
   )
 }
 
