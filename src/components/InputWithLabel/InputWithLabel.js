fc166aa3412aa91c57fb8a1445b971f4f8a3754e
alex <hamster2240@gmail.com>
2018-11-10 18:24:55 +0300

    Add inputClass variable to InputWithLabel component

--- src/components/InputWithLabel/InputWithLabel.js
+++ src/components/InputWithLabel/InputWithLabel.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './InputWithLabel.css';
 
-const InputWithLabel = ({ name, type, labelText, placeholder }) => {
+const InputWithLabel = ({ name, type, labelText, placeholder, inputClass }) => {
   return (
     <div>
       <label htmlFor={name} className="regForm-label">{labelText}</label>
-      <input type="type" id="firstName" name={name} className="input-with-label"  placeholder={placeholder}/>
+      <input type="type" id="firstName" name={name} className={inputClass}  placeholder={placeholder}/>
     </div>
   );
 }
