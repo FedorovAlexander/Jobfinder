56753a4e78299c3a3dae070e5e1d587474036896
alex <hamster2240@gmail.com>
2018-11-10 16:27:36 +0300

    ChartPageFooter markup and styles

--- src/components/Footer/ChartPageFooter.js
+++ src/components/Footer/ChartPageFooter.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ChartPageFooter.css';
-import Link from '../Link/Link';
+import LinkWithImg from '../LinkWithImg/LinkWithImg';
+
+const assets = require('../../assets.js');
 
 const ChartPageFooter = ({ text }) => {
 
   return (
-    <footer className="page-footer">
-      <div className="page-footer__container">
-        <p className="page-footer__text"></p>
-        <Link />
+    <footer className="chart-page-footer">
+      <div className="chart-page-footer__container">
+        <Link to="/new-vacancy" className="chart-page-footer__add-link">
+        <LinkWithImg
+          text='Add a New Vacancy'
+          textClass="chart-page-footer__add-text"
+          imagePath={assets.addIcon}
+          linkClass="chart-page-footer__add-link"
+          imgClass="chart-page-footer__add-img"
+          imgAlt="add"/>
+        </Link>
       </div>
     </footer>
   );
