1b379e3f9e64abe25fa1883eb7753ee898dd561f
alex <hamster2240@gmail.com>
2018-11-11 20:17:21 +0300

    Login component refactoring

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import InputWithLabel from '../InputWithLabel/InputWithLabel';
 import FormButton from '../FormButton/FormButton';
 import Link from '../Link/Link';
 import '../LoginForm/LoginForm.css';
 
-const LoginForm = () => {
-  return (
-    <div>
-      <section className="log-form">
-        <h1 className="log-form__title">Log In</h1>
-        <form>
-          <InputWithLabel
-            type="email"
-            name="email"
-            placeholder="Email"
-            inputClass="input-with-label input-with-label--login-reg"
-          />
-          <InputWithLabel
-            type="password"
-            name="password"
-            placeholder="Password" inputClass="input-with-label input-with-label--login-reg"
-          />
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        <section className="log-form">
+          <h1 className="log-form__title">Log In</h1>
+          <form>
+            <InputWithLabel
+              type="email"
+              name="email"
+              placeholder="Email"
+              inputClass="input-with-label input-with-label--login-reg"
+            />
+            <InputWithLabel
+              type="password"
+              name="password"
+              placeholder="Password" inputClass="input-with-label input-with-label--login-reg"
+            />
 
-          <FormButton
-            buttonClass="log-form__sumbit"
-            text="Submit" path="/chartPage"
-          />
-          <Link text="Do not have an account?" linkClass="log-form__forgot" path="/registration"/>
-        </form>
+            <FormButton
+              onSubmit
+              buttonClass="log-form__sumbit"
+              text="Submit" path="/chartPage"
+            />
+            <Link text="Do not have an account?" linkClass="log-form__forgot" path="/registration"/>
+          </form>
 
-      </section>
-    </div>
-  )
+        </section>
+      </div>
+    )
+  }
 }
 
 export default LoginForm;
