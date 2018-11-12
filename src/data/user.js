8576f513efdb93d967bd7f7dfdfc166eb949aa90
alex <hamster2240@gmail.com>
2018-11-12 12:05:42 +0300

    Add users data file

---
+++ src/data/user.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    "login": "clarkkent",
+    "password": "1234"
+    },
+  {
+    "login": "brucewayne",
+    "password": "1234"
+    },
+    {
+    "login": "wonderwoman",
+    "password": "1234"
+    }
+]
