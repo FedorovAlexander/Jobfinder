f9375b4eb7b601a89b177610917d0debfcf51b64
alex <hamster2240@gmail.com>
2018-11-14 19:51:39 +0300

    Make card component a class component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import './Card.css';
+// { position, company, salary, status, goToVacancy, vacancyStatus }
+class Card extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+        
+    };
 
-const Card = ({ position, company, salary, status, goToVacancy, vacancyStatus }) => {
-
-  return (
-    <div className="card">
-      <div className="card__img-block">
-        {position.charAt(0)}
-      </div>
-      <div className="card__position-company-block">
-        <h2 className="card__position">
-          <Link to="/vacancy" onClick={goToVacancy}>
-            <div className="card__link card__link--position">
-              {position}
+  }
+  render() {
+    return (
+      <div className="card">
+        <div className="card__img-block">
+          {this.props.position.charAt(0)}
+        </div>
+        <div className="card__position-company-block">
+          <h2 className="card__position">
+            <Link to="/vacancy" onClick={this.props.goToVacancy}>
+              <div className="card__link card__link--position">
+                {this.props.position}
+              </div>
+            </Link>
+          </h2>
+        <div className="card__company">
+          <Link to="/company">
+            <div className="card__link card__link--company">
+            {this.props.company}
             </div>
           </Link>
-        </h2>
-      <div className="card__company">
-        <Link to="/company">
-          <div className="card__link card__link--company">
-          {company}
-          </div>
-        </Link>
+        </div>
+        </div>
+        <div className="card__salary-block">
+          <span className="card__salary">{"$" + this.props.salary + "/year"}</span>
+          {/* <span className="card__status">{this.props.vacancyStatus}</span> */}
+        </div>
       </div>
-      </div>
-      <div className="card__salary-block">
-        <span className="card__salary">{"$" + salary + "/year"}</span>
-        {/* <span className="card__status">{vacancyStatus}</span> */}
-      </div>
-    </div>
-  )
+    )
+  }
+ 
 }
 
 export default Card;
