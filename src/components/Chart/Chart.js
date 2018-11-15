fff5583e61a8daa5782e576fda21600b20e40a20
alex <hamster2240@gmail.com>
2018-11-15 12:38:14 +0300

    Add id to cards

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -27,6 +27,7 @@ class Chart extends Component {
       <PageTitle titleClass="page-title" titleText="Chart" />
       {!userName ? <div className="login-page"><Link to="/login">Log In</Link> to See Your Job Chart</div> : this.state.pageOfItems.map((item, index) => (
         <Card key={Math.random()}
+          id={item.id}
           position={item.position}
           company={item.companyName}
           salary={item.salary}
