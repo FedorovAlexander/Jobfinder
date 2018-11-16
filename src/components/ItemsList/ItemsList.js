7953acdbe255713fc8ab9dceccb2e21d829a20c6
alex <hamster2240@gmail.com>
2018-11-16 13:07:08 +0300

    Make ItemsList a class Component. Add props from Card component

--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "./ItemsList.css";
 import ListItem from "../ListItem/ListItem";
+import {vacancies} from '../../data/vacancies';
 const assets = require('../../assets');
 
-const ItemsList = () => {
-  return (
-    <section className="conditions">
-      <h2 className="conditions__title">Conditions</h2>
-      <ul className="conditions__list">
-        <ListItem imgSrc={assets.salaryIcon} itemTitle="Salary" itemValue="value" />
-        <ListItem imgSrc={assets.statusIcon} itemTitle="Status" itemValue="value" />
-        <ListItem imgSrc={assets.timeIcon} itemTitle="Work Time" itemValue="value" />
-        <ListItem imgSrc={assets.roadIcon} itemTitle="Road Time" itemValue="value" />
-        <ListItem imgSrc={assets.educationIcon} itemTitle="Education" itemValue="value" />
-        <ListItem imgSrc={assets.intIcon} itemTitle="Interesting" itemValue="value" />
-      </ul>
-    </section>
-  )
+
+class ItemsList extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      id: props.id
+    }
+  }
+  render() {
+    return (
+      <section className="conditions">
+        <h2 className="conditions__title">Conditions</h2>
+        <ul className="conditions__list">
+          <ListItem
+           imgSrc={assets.salaryIcon}
+           itemTitle="Salary"
+           itemValue={"$" + vacancies[this.state.id].salary}
+           />
+          <ListItem
+          imgSrc={assets.statusIcon}
+          itemTitle="Status"
+          itemValue={vacancies[this.state.id].status} />
+          <ListItem
+          imgSrc={assets.timeIcon}
+          itemTitle="Work Time"
+          itemValue={
+            Math.floor(vacancies[this.state.id].minutes/60) + 'h ' + Math.floor(vacancies[this.state.id].minutes/60) + 'm'
+            }
+          />
+          <ListItem
+          imgSrc={assets.roadIcon}
+          itemTitle="Road Time"
+          itemValue={vacancies[this.state.id].roadTime }
+          />
+          <ListItem
+          imgSrc={assets.educationIcon}
+          itemTitle="Education"
+          itemValue={vacancies[this.state.id].requiresEducation === true ? "Requires": "Not Requires"}
+          />
+          <ListItem
+          imgSrc={assets.intIcon}
+          itemTitle="Interesting"
+          itemValue={vacancies[this.state.id].interesting === true ? "Yes" : "No"}
+          />
+        </ul>
+      </section>
+    )
+  }
 }
 
 export default ItemsList;
