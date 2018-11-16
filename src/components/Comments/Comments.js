b528eb46474a304aeafebb6680bc384704b4362b
alex <hamster2240@gmail.com>
2018-11-16 12:56:26 +0300

    Remove test text from Comments.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,7 +5,7 @@ function Comments({ commentsText }) {
   return (
       <section className="comments">
         <h2 className="coditions__item-title">Comments</h2>
-        <p className="comments__item">Test{commentsText}</p>
+        <p className="comments__item">{commentsText}</p>
       </section>
   )
 }
