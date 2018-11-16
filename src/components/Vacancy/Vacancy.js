05810ac3c96a2d4da69f0c8038b735bb5042daf6
alex <hamster2240@gmail.com>
2018-11-16 13:11:07 +0300

    Add pros from card to Vacancy component

--- src/components/Vacancy/Vacancy.js
+++ src/components/Vacancy/Vacancy.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from '../Header/Header';
 import VacancyContent from '../VacancyContent/VacancyContent';
 import Footer from '../Footer/Footer';
 import './Vacancy.css';
 
 
-const Vacancy = () => {
-  return (
-    <div>
-      <Header />
-        <section className="vacancy">
-          <div className="vacancy__wrapper">
-            <div className="vacancy__container">
-              <VacancyContent />
+class Vacancy extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: props.id
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+          <section className="vacancy">
+            <div className="vacancy__wrapper">
+              <div className="vacancy__container">
+                <VacancyContent id={this.props.location.state.id}/>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-
-      </section>
-
-    </div>
-  )
+        </section>
+      </div>
+    )
+  }
 }
 
 export default Vacancy;
