453c8f9ff77f45eafff192f21415559d43d812a6
alex <hamster2240@gmail.com>
2018-11-16 13:08:39 +0300

    Add props from card to the VacancyContent

--- src/components/VacancyContent/VacancyContent.js
+++ src/components/VacancyContent/VacancyContent.js
@@ -5,32 +5,30 @@ import ItemsList from '../ItemsList/ItemsList';
 import Comments from '../Comments/Comments';
 import PageTitle from '../PageTitle/PageTitle';
 //{vacancyTitle, vacancyDescription, company}
-
+import {vacancies} from '../../data/vacancies'
 
 
 class VacancyContent extends Component {
   constructor(props) {
     super()
     this.state = {
-    
+      id: props.id
     }
-
   }
-  render(props) {
+
+  render() {
     return (
       <section className="vacancy-content">
-            <PageTitle titleClass="page-title page-title--vacancy" titleText="" />
+            <PageTitle titleClass="page-title page-title--vacancy" titleText={vacancies[this.state.id].position} />
             <Link to="/company" className="vacancy-content__company-link">
               <div className="card__link card__link--company">
-              {""} company
+              {vacancies[this.state.id].companyName}
               </div>
             </Link>
             <h2 className="vacancy-content__desc-title">Job Description</h2>
-            <p className="vacancy-content__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor neque, suscipit ac accumsan eget, sollicitudin sit amet urna. Mauris ut ex risus. Aliquam eleifend augue diam, eget congue ex bibendum vitae. Nulla condimentum maximus nisl non venenatis. Fusce vitae dui erat. Suspendisse in consectetur libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-  
-            Quisque feugiat ut orci ac pretium. Vestibulum vitae mi iaculis, mattis metus at, commodo libero. Vivamus at placerat nulla, nec lobortis arcu. Phasellus ultrices quam sed dolor eleifend, sed interdum est pharetra. Donec nec dolor eu nisl auctor ullamcorper. Vestibulum laoreet erat sit amet ultricies tristique. Vestibulum eu dolor volutpat, malesuada nibh eget, pulvinar ante. Vivamus euismod mattis erat in eleifend. Morbi et dui vitae libero volutpat aliquet a quis ipsum. Maecenas iaculis augue nec lacus aliquam, sit amet posuere dolor ullamcorper. Nullam ac aliquam elit, ut molestie diam.</p>
-            <ItemsList />
-            <Comments />
+            <p className="vacancy-content__description">{vacancies[this.state.id].jobDescription}</p>
+            <ItemsList id={this.state.id}/>
+            <Comments commentsText={vacancies[this.state.id].comment}/>
       </section>
     )
   }
