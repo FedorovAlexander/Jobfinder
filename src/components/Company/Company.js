7e5b9fbef8fad63116e61e44584355f86e2c2e4a
alex <hamster2240@gmail.com>
2018-11-16 14:00:23 +0300

    Add prop to chow company content

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -4,7 +4,13 @@ import Footer from '../Footer/Footer';
 import CompanyContent from '../CompanyContent/CompanyContent';
 import './Company.css';
 
-class Company extends Component {
+class Company extends Component { 
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: props.id
+    }
+  }
   render() {
     return (
         <div>
@@ -12,7 +18,7 @@ class Company extends Component {
           <section className="company">
             <div className="company__wrapper">
               <div className="company__container">
-          <CompanyContent />
+          <CompanyContent id={this.props.location.state.id}/>
         </div>
         <Footer />
       </div>
--- src/components/CompanyContent/CompanyContent.js
+++ src/components/CompanyContent/CompanyContent.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './CompanyContent.css';
-import PageTitle from '../PageTitle/PageTitle'
+import PageTitle from '../PageTitle/PageTitle';
+import {vacancies} from '../../data/vacancies'
 
-function CompanyContent ({vacancyTitle, vacancyDescription, company}) {
-  return (
-    <section className="company-content">
-          <PageTitle titleClass="page-title page-title--company" titleText="Company" />
-
-          <h2 className="company-content__desc-title">About</h2>
-          <p className="company-content__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor neque, suscipit ac accumsan eget, sollicitudin sit amet urna. Mauris ut ex risus. Aliquam eleifend augue diam, eget congue ex bibendum vitae. Nulla condimentum maximus nisl non venenatis. Fusce vitae dui erat. Suspendisse in consectetur libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-
-          Quisque feugiat ut orci ac pretium. Vestibulum vitae mi iaculis, mattis metus at, commodo libero. Vivamus at placerat nulla, nec lobortis arcu. Phasellus ultrices quam sed dolor eleifend, sed interdum est pharetra. Donec nec dolor eu nisl auctor ullamcorper. Vestibulum laoreet erat sit amet ultricies tristique. Vestibulum eu dolor volutpat, malesuada nibh eget, pulvinar ante. Vivamus euismod mattis erat in eleifend. Morbi et dui vitae libero volutpat aliquet a quis ipsum. Maecenas iaculis augue nec lacus aliquam, sit amet posuere dolor ullamcorper. Nullam ac aliquam elit, ut molestie diam.</p>
-    </section>
-  )
+class CompanyContent extends Component { 
+  constructor(props) {
+    super()
+    this.state = {
+      id: props.id
+    }
+  }
+  render() {
+    return (
+      <section className="company-content">
+            <PageTitle titleClass="page-title page-title--company" titleText={vacancies[this.state.id].companyName} />
+  
+            <h2 className="company-content__desc-title">About</h2>
+            <p className="company-content__description">{vacancies[this.state.id].description}</p>
+      </section>
+    )
+  }
 }
 
 export default CompanyContent;
