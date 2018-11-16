146783739ad101ce7202de7ffb868c2088c0e18f
alex <hamster2240@gmail.com>
2018-11-16 15:13:13 +0300

    Create vacancy page footer component

---
+++ src/components/Footer/VacancyPageFooter.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import './VacancyPageFooter.css';
+import LinkWithImg from '../LinkWithImg/LinkWithImg';
+const userName = sessionStorage.getItem('login');
+const assets = require('../../assets.js');
+// ({ text }) =>
+class VacancyPageFooter extends Component  {
+  constructor(props) {
+    super()
+    this.state = {
+      id: props.id
+    }
+  }
+
+  render() {
+    return (
+      <footer className="vacancy-page-footer">
+        <div className="vacancy-page-footer__container">
+          {!userName ? <p className="page-footer__text"></p> : 
+  
+          <Link 
+          className="vacancy-page-footer__edit-link" 
+          to={{
+          pathname: "/edit-vacancy",
+          state: {id: this.state.id}
+          }} 
+          >
+          <LinkWithImg
+            text='Edit Vacancy'
+            textClass="vacancy-page-footer__edit-text"
+            imagePath={assets.editIcon}
+            linkClass="vacancy-page-footer__edit-link"
+            imgClass="vacancy-page-footer__edit-img"
+            imgAlt="edit"/>
+          </Link>
+          }
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default VacancyPageFooter;
