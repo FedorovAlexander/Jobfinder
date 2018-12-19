e4b2aae30f1e481aff80263e960c17433a78343c
alex <hamster2240@gmail.com>
2018-12-19 20:51:38 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jobfinder/precache-manifest.c2facdee7cf58f9cca12234fe38cdcef.js"
+  "/Jobfinder/precache-manifest.b50033d53ee0051da598a4c0d33f8d6e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/jobfinder/index.html", {
+workbox.routing.registerNavigationRoute("/Jobfinder/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
